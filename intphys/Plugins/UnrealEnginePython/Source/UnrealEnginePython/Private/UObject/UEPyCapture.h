a327b7f7c837e8ea54cc69ea0bdd5762b08080a9
Mathieu Bernard <mathieu_bernard@laposte.net>
2017-09-13 22:42:46 +0200

    UnrealEnginePython as submodule

--- intphys/Plugins/UnrealEnginePython/Source/UnrealEnginePython/Private/UObject/UEPyCapture.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-#include "UnrealEnginePython.h"
-
-
-PyObject *py_ue_capture_initialize(ue_PyUObject *, PyObject *);
-PyObject *py_ue_capture_start(ue_PyUObject *, PyObject *);
-PyObject *py_ue_capture_stop(ue_PyUObject *, PyObject *);
