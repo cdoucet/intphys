23ff28fd082a10594f88b2de000d4ed8876c3de7
Mathieu Bernard <mathieu_bernard@laposte.net>
2017-09-20 16:57:08 +0200

    Playing with Python and camera

--- Content/Scripts/camera.py
+++ Content/Scripts/camera.py
@@ -1,8 +1,17 @@
+"""The camera.py module models the camera behavior.
+
+"""
+
 import unreal_engine as ue
 
 
 class Camera:
+    def begin_play(self):
+        self.camera = self.uobject.get_owner()
+        print(self.camera.get_name())
+        print(self.uobject.get_name())
+
     def on_actor_begin_overlap(self, me, other_actor):
         raise RuntimeError(
             'Overlap detected between {} and {}',
-            me.uobject.get_name(), other_actor.get_name())
+            me.uobject.get_name(), other_actor.uobject.get_name())
