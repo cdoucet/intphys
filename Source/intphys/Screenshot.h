b470cc2428f4224a29f960f7eb8aab86dca94c41
Mathieu Bernard <mathieu_bernard@laposte.net>
2018-02-12 23:51:03 +0100

    WIP

---
+++ Source/intphys/Screenshot.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Screenshot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTPHYS_API UScreenshot : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
